8d19002c5c1bd5acf74793cb0a1687ae9fc11f05
igorvilches <42453166+igorvilches@users.noreply.github.com>
2018-08-20 20:41:26 -0300

    Add files via upload

---
+++ codigoaula2.R
@@ -0,0 +1,6 @@
+x<-1:4
+y <- seq(from = 1,to = 10,by = 1) #sequencia de 1 em 1 de 1 a 10
+z <- rep(3,10)                    #numero , vezes
+ls()                              #mostra minha lista de variaveis
+a <- 1:20
+
